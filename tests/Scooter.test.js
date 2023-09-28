183721dbaeefb48855f1407d88ca2cde7a7dfddf
zoemx <146032816+zoemx@users.noreply.github.com>
2023-09-28 16:57:28 -0500

    Initial commit

---
+++ tests/Scooter.test.js
@@ -0,0 +1,25 @@
+const Scooter = require('../src/Scooter')
+const User = require('../src/User')
+
+//typeof scooter === object
+describe('scooter object', () => {
+  test('Scooter class should create Scooter instance', () => {
+    const scooter = new Scooter();
+    expect(scooter).toBeInstanceOf(Scooter);
+  });
+})
+
+//Method tests
+describe('scooter methods', () => {
+  // tests here!
+
+  //rent method
+  
+
+  //dock method
+
+  //requestRepair method
+
+  //charge method
+
+})
---
+++ tests/ScooterApp.test.js
@@ -0,0 +1,22 @@
+const Scooter = require('../src/Scooter')
+const User = require('../src/User')
+const ScooterApp = require('../src/ScooterApp')
+
+const scooterApp = new ScooterApp();
+// ScooterApp tests here
+
+// register user
+describe("registerUser method tests", () => {
+  test("Should return instance of User", () => {
+    let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+    expect(response).toBeInstanceOf(User);
+  });
+});
+
+// log in
+
+// log out
+
+// rent scooter
+
+// dock scooter
