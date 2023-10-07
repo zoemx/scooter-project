9afc8aac723fa60b3beca88130f251e37335c1f7
zoemx <146032816+zoemx@users.noreply.github.com>
2023-10-07 02:57:17 -0500

    Update User.js

--- src/User.js
+++ src/User.js
@@ -1,5 +1,24 @@
 class User {
   // User code here
+  constructor (username,password,age,loggedIn == false){
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = loggedIn;
+
+  }
+
+  login(password){
+    if (password == true){
+      this.loggedIn = true;
+    }else{
+      throw new Error ("Incorrect Password");
+    }
+  }
+
+  logout(){
+    this.loggedIn = false;
+    }
 }
 
 module.exports = User
