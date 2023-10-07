a4186e093c2e2181d07a757e8295154e9766a6aa
zoemx <146032816+zoemx@users.noreply.github.com>
2023-10-07 02:57:56 -0500

    Update Scooter.js

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,32 @@
 class Scooter{
   // scooter code here
+  constructor (station,user = null, serial = 0, nextSerial = serial+1, charge = 100, isBroken = false){
+    //user,serial, nextSerial, charge, isBroken
+    this.station = station;
+    this.user = user;
+    this.serial = serial;
+    this.nextSerial = next.serial;
+    this.charge = charge;
+    this.isBroken = isBroken;
+
+  }
+  rent(user){
+    if (this.charge > 20 && isBroken == false){
+      return this.user
+    }else{
+      throw new Error ("Scooter needs to be charged or repaired");
+    }
+  }
+
+  dock(station){
+    this.user = null;
+    return this.station
+  }
+
+ 
+
 }
 
 
 module.exports = Scooter
+
